a4c65cea93612054cfb7e2699687d81650d357eb
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-08-06 08:55:26 +0900

    Final css fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  # show all users except current user
   def index
     @users = User.where.not(id: current_user.id)
   end
